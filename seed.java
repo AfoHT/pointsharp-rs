e792a2adf0a80316953befa4e210ba3c4f6740d0
Henrik Tjäder <henrik@tjaders.com>
2022-03-24 22:42:27 +0100

    PointSharp HOTP tool
    Also supporting oathtool and other standard base32 tools

---
+++ seed.java
@@ -0,0 +1,50 @@
+import java.io.FileNotFoundException; 
+import java.io.PrintStream;
+  class PointSharpSeed{  
+    public static void main(String args[]) throws FileNotFoundException  {  
+
+
+        String userseed = "539787";
+        long value = 123456789L;
+
+        int[] iIncrementalCounter_int = new int[8]; 
+
+        iIncrementalCounter_int[7] = (int)(value & 0xFFL);
+        iIncrementalCounter_int[6] = (int)(value >> 8 & 0xFFL);
+        iIncrementalCounter_int[5] = (int)(value >> 16 & 0xFFL);
+        iIncrementalCounter_int[4] = (int)(value >> 24 & 0xFFL);
+        iIncrementalCounter_int[3] = (int)(value >> 32 & 0xFFL);
+        iIncrementalCounter_int[2] = (int)(value >> 40 & 0xFFL);
+        iIncrementalCounter_int[1] = (int)(value >> 48 & 0xFFL);
+        iIncrementalCounter_int[0] = (int)(value >> 56 & 0xFFL);
+
+        byte[] iSeed;
+        iSeed = new byte[20];
+        byte[] pad = new byte[]{-39, -99, -69, -12, -7, -105, 24, -17, -2, 37, -3, 0, 125, -4, -32, 107, -5, 80, 48, -91};
+        System.arraycopy(pad, 0, iSeed, 0, 20);
+
+        for (int i = 0; i < userseed.length(); ++i) {
+            iSeed[i] = (byte)userseed.charAt(i);
+        }
+        System.out.println("Userseed");  
+        System.out.println(userseed);  
+        System.out.println("Incremental Counter");  
+
+        StringBuilder out = new StringBuilder();
+        for (int i : iIncrementalCounter_int) {
+            out.append(String.format("%d ", i));
+        }
+        System.out.println(out.toString());
+
+        System.out.println("iSeed");  
+
+        StringBuilder sb = new StringBuilder();
+        for (byte b : iSeed) {
+            sb.append(String.format("%02X", b));
+        }
+        System.out.println(sb.toString());
+
+        PrintStream p=new PrintStream("javaoutfile.byte");
+        p.write(iSeed, 0, 20);
+    }  
+}
---
+++ src/main.rs
@@ -0,0 +1,112 @@
+//! `PointSharp HOTP`
+//!
+//! Usage: From the `PointSharp` portal retrieve the 6 digit activation code
+//! then:
+//!
+//! ```shell
+//! pointsharp <ACTIVATIONCODE> [COUNT]
+//! ```
+//! and use the resulting OTP value for logging in.
+//!
+//! Run with --help for a full list of options
+//!
+//! The base32 encoded HOTP SECRET from `--oathmode` can be used by not only `oathtool`,
+//! but all standard HOTP capable tools such as Aegis, Google Authenticator, Microsoft Authenticator etc.
+//!
+//! The default counter value is 123456789, and should be incremented by 1
+//! for each authentication attempt, see general [HOTP](https://datatracker.ietf.org/doc/html/rfc4226)
+//! functionality.
+//!
+//! Example HOTP:
+//! `activation_code` = "539787";
+//! Count = 1, OTP = 750 277
+//! Count = 2, OTP = 962 814
+//! Count = 3, OTP = 331 876
+//! Count = 4, OTP = 950 412
+//!
+use ootp::hotp::{Hotp, MakeOption};
+
+pub use clap::{ArgEnum, ArgGroup, Command, CommandFactory, ErrorKind, Parser, Subcommand};
+
+/// Simple program to calculate pointsharp HOTP codes
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+pub struct Cli {
+    /// If set activate oathtool mode,
+    /// useful with command substitution oathtool $(pointsharp -o 123456)
+    #[clap(short, long)]
+    pub oathmode: bool,
+
+    /// HOTP window, how many values to print
+    #[clap(short, long)]
+    pub window: Option<u32>,
+
+    /// HOTP Secret, 6 digits
+    activation_code: String,
+
+    /// HOTP counter
+    pub count: Option<u32>,
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    // From the Java application, converted from i8 to u8
+    let mut seed: [u8; 20] = [
+        0xD9, 0x9D, 0xBB, 0xF4, 0xF9, 0x97, 0x18, 0xEF, 0xFE, 0x25, 0xFD, 0x00, 0x7D, 0xFC, 0xE0,
+        0x6B, 0xFB, 0x50, 0x30, 0xA5,
+    ];
+
+    // Default counter start value used by PointSharp
+    let mut counter: u32 = 123_456_789;
+
+    let activation_code = cli.activation_code;
+
+    if let 1..=6 = activation_code.len() {
+    } else {
+        let mut cmd = Cli::command();
+        cmd.error(
+            ErrorKind::InvalidValue,
+            "Must provide the 6 digit activation code",
+        )
+        .exit();
+    }
+
+    // Modify default seed with the activation code
+    seed_add_activation_code(&mut seed, &activation_code);
+    // Default minimum PointSharp counter added to user requested HOTP count
+    counter += cli.count.unwrap_or(0);
+
+    if cli.oathmode {
+        // Activate oathtool base32 mode
+        print!("--base32 {}", base32_encode(&seed));
+        // Emit oathtool counter flag
+        print!(" --counter {}", counter);
+    } else {
+        // Provide OTP directly via OOTP crate
+        let seed_str = unsafe { std::str::from_utf8_unchecked(&seed) };
+        let hotp = Hotp::new(seed_str);
+        if let Some(w) = cli.window {
+            // Print multiple consecutive OTP values
+            for i in 0..w {
+                let code = hotp.make(MakeOption::Counter(u64::from(counter + i)));
+                println!("{}", code);
+            }
+        } else {
+            let code = hotp.make(MakeOption::Counter(u64::from(counter)));
+            println!("{}", code);
+        }
+    }
+}
+
+fn seed_add_activation_code(seed: &mut [u8], activation_code: &str) {
+    // Copy the provided characters into the seed,
+    // overwriting existing bytes
+    for (i, c) in activation_code.chars().enumerate() {
+        seed[i] = c as char as u8;
+    }
+}
+
+fn base32_encode(secret: &[u8]) -> String {
+    base32::encode(base32::Alphabet::RFC4648 { padding: false }, secret)
+}
